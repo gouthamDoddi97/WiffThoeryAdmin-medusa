12ea1f4742015bdfeb1780e1c69d72e09fb9923f
Goutham-zern <goutham@zern.ai>
2026-04-01 17:10:33 +0400

    refactor: update email notifications module structure to use services array

--- src/modules/email-notifications/index.ts
+++ src/modules/email-notifications/index.ts
@@ -1,8 +1,7 @@
 import NodemailerNotificationService from "./service"
-import { Module } from "@medusajs/framework/utils"
 
 export const EMAIL_NOTIFICATIONS_MODULE = "emailNotifications"
 
-export default Module(EMAIL_NOTIFICATIONS_MODULE, {
-  service: NodemailerNotificationService,
-})
+export default {
+  services: [NodemailerNotificationService],
+}
